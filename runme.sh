3e9228707fb3a096a181d169bf1849099f4fa0f4
Harry45 <arrykrish@gmail.com>
2023-09-19 11:30:04 +0100

    runme script

--- runme.sh
+++ runme.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
-python main.py --config=config.py:planck-2018 --config.sampling.nsamples=10 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors
+which python
+python main.py --config=config.py:planck-2018 --config.sampling.nsamples=1 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors
 
 # run script with the following
 # addqueue -m 16 -n 1x8 -s ./runme.sh
