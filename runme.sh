5bb61cd6a5e895960e288e59a405cba528e7b0dc
Harry45 <arrykrish@gmail.com>
2023-09-19 11:42:29 +0100

    runme

--- runme.sh
+++ runme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
-conda activate base
+conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
 python main.py --config=config.py:planck-2018 --config.sampling.nsamples=1 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors
