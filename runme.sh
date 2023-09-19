44329c5217a666470e36dc66574d8245f92f9ed8
Harry45 <arrykrish@gmail.com>
2023-09-19 11:38:25 +0100

    runme script

--- runme.sh
+++ runme.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
+conda activate base
 which python
+echo $(for i in $(seq 1 50); do printf "-"; done)
 python main.py --config=config.py:planck-2018 --config.sampling.nsamples=1 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors
 
 # run script with the following
