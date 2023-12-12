bab908629f0cf2cc02c05428374c8b8248cf5a41
Harry45 <arrykrish@gmail.com>
2023-12-12 09:13:52 +0000

    experiment

--- runme.sh
+++ runme.sh
@@ -3,7 +3,7 @@
 # conda activate jaxcosmo
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
-python main.py --config=config.py:planck-2018 --config.sampling.nsamples=10000 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors_accurate
+python main.py --config=config.py:planck-2018 --config.sampling.nsamples=10 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors_accurate
 
 # run script with the following
 # addqueue -m 16 -n 1x8 -s ./runme.sh
