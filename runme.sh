bc608a34f833bfbaf58893dba8ff2f219c7d2ff8
Harry45 <arrykrish@gmail.com>
2024-03-05 16:48:42 +0000

    experiment

--- runme.sh
+++ runme.sh
@@ -1,9 +1,12 @@
-# #!/bin/bash
-# source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
-# conda activate jaxcosmo
+#!/bin/bash
+module load cuda/12.3
+source /mnt/zfsusers/phys2286/anaconda3/etc/profile.d/conda.sh
+source activate jaxcosmo
+echo $PWD
 which python
 echo $(for i in $(seq 1 50); do printf "-"; done)
-python main.py --config=config.py:planck-2018 --config.sampling.nsamples=10000 --config.sampling.uniform_prior=True --config.sampling.fname=planck_priors_accurate_2
+python3 -m src.emulike.planck.main --config=src/emulike/planck/config.py:planck-2018 \
+    --config.sampling.nsamples=1 --config.sampling.fname=test_1 --config.sampling.uniform_prior=True
 
 # run script with the following
 # addqueue -m 16 -n 1x8 -s ./runme.sh
