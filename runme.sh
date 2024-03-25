c4a13d3e7f8607b7e31547db2d31e8d965bbb4b9
Harry45 <arrykrish@gmail.com>
2024-03-25 09:34:32 +0000

    JLA experiment

--- runme.sh
+++ runme.sh
@@ -7,6 +7,6 @@ echo $(for i in $(seq 1 50); do printf "-"; done)
 # python3 -m src.emulike.planck.main --config=src/emulike/planck/config.py:planck-2018 \
 #     --config.sampling.nsamples=10000 --config.sampling.fname=experiment_1
 python3 -m src.emulike.jla.main --config=src/emulike/jla/config.py:jla \
-    --config.sampling.nsamples=10000 --config.sampling.fname=experiment_1
+    --config.sampling.nsamples=10000 --config.sampling.fname=experiment_2
 # run script with the following
 # addqueue -m 16 -n 1x8 -s ./runme.sh
